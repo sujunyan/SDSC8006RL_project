30c51c0e7d33cebbc5c06126cd32c7ec6ed7e4a7
Junyan Su <junyan.su@my.cityu.edu.hk>
2021-03-24 17:32:10 +0800

    working on RLAgents

--- RLAgents.py
+++ RLAgents.py
@@ -1,5 +1,12 @@
+from game import Agent
+from util import manhattanDistance
+from game import Directions
+import random, util
+
+
+
 """
-From this part, we start to implement our agent with Reinforcement Learning technique
+In this file, we implement our agent with Reinforcement Learning technique
 """
 
 class RLAgent(Agent):
@@ -12,22 +19,42 @@ class RLAgent(Agent):
     is another abstract class.
     """
 
-    def __init__(self, evalFn = 'scoreEvaluationFunction', depth = '2'):
+    def __init__(self, numTraining = 0):
         self.index = 0 # Pacman is always agent index 0
+        self.numTraining = numTraining
         #self.evaluationFunction = util.lookup(evalFn, globals())
         #self.depth = int(depth)
+
+    def convertState(self, gameState):
+        """
+        The original hash function of gameState including scores which is useless in our MDP setting. So in this function, we convert the gameState to a tuple of data we really care.
+        """
+        return (gameState.getPacmanPosition(),
+                gameState.getGhostPositions(),
+                gameState.getCapsules(),
+                gameState.getFood())
 
 
 class MonteCarloAgent(RLAgent):
     """
     Use Monte Carlo Control to play the pacman game
+    Refer to the lesson slide 6: Model free control (11-19)
     """
 
-    def __init__(self):
-        super().__init__() # call initialize function of the parent class
+    def __init__(self, numTraining = 0, eps=1e-2):
+        super().__init__(numTraining=numTraining) # call initialize function of the parent class
+        # the pol
+        self.eps = eps # The parameter used in \epsilon greedy exploration
+        print(self.eps)
+        self.pi = {}
+        self.Q = {}
 
     def getAction(self, gameState):
         """
         """
+        print(self.convertState(gameState))
         util.raiseNotDefined()
+
+    
+        
 
